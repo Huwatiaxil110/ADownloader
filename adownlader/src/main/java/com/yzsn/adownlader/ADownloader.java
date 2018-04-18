49b0faafd42ed513cd63e36cc41a8328dfb9e92d
zhu.cong <zhu.cong04902@westone.com.cn>
2018-04-18 18:31:21 +0800

    create

---
+++ adownlader/src/main/java/com/yzsn/adownlader/ADownloader.java
@@ -0,0 +1,9 @@
+package com.yzsn.adownlader;
+
+/**
+ * Created by zc on 2018/4/18.
+ */
+
+public class ADownloader {
+
+}
---
+++ adownlader/src/main/java/com/yzsn/adownlader/utils/FileUtil.java
@@ -0,0 +1,8 @@
+package com.yzsn.adownlader.utils;
+
+/**
+ * Created by zc on 2018/4/18.
+ */
+
+public class FileUtil {
+}
